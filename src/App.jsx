747fb5836eddebc11476f3bf3ac76eab7b895dc4
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-09-19 00:27:50 +0200

    something do

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  const API_KEY = '3e3ad4fe8ef93a013732290a26488529';
+  const URL =
+    'https://api.themoviedb.org/3/movie/550?api_key=3e3ad4fe8ef93a013732290a26488529';
+
+  return <div className="App"></div>;
+}
+
+export default App;
