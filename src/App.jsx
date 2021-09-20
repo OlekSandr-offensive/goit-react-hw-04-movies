568105bcf77a5be8e503122e50fc3c51a081ffe8
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-09-20 22:30:03 +0200

    add loader-spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import AppBar from './components/AppBar/AppBar';
 import Container from './components/Container/Container';
 import HomePage from './components/HomePage/HomePage';
@@ -6,13 +8,17 @@ import MovieDetailsPage from './components/MovieDetailsPage/MovieDetailsPage';
 import Cast from './components/Cast/Cast';
 import Reviews from './components/Reviews/Reviews';
 
+const loader = (
+  <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={3000} />
+);
+
 function App() {
   return (
     <Container>
       <AppBar />
       <Switch>
         <Route path="/" exact>
-          <HomePage />
+          <HomePage loader={loader} />
         </Route>
         <Route path="/movies"></Route>
       </Switch>
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { fetchTrendingMovies } from '../../fetch-service';
 // import * as homePageAPI from '../../fetch-service';
@@ -12,43 +12,51 @@ const Status = {
   REJECTED: 'rejected',
 };
 
-export default function HomePage() {
+export default function HomePage({ loader }) {
+  const { url } = useRouteMatch();
   const [trendMovies, setTrendMovies] = useState([]);
   const [error, setError] = useState(null);
+  const [spinner, setSpinner] = useState(false);
   const [status, setStatus] = useState(Status.IDLE);
 
   useEffect(() => {
     // homePageAPI.fetchTrendingMovies().then(setTrendMovies);
     const fetchMovies = async function () {
       try {
+        setSpinner(true);
         const data = await fetchTrendingMovies();
+        setStatus(Status.RESOLVED);
         setTrendMovies(date => [...date, ...data.results]);
       } catch (error) {
-        // setStatus(Status.REJECTED);
+        setStatus(Status.REJECTED);
         setError(error);
+      } finally {
+        setSpinner(false);
       }
     };
-    const promise = fetchMovies();
-    promise.then(result => console.log(result));
+
+    fetchMovies();
   }, []);
 
   return (
     <>
       <h1>Trending today</h1>
-
-      <ul>
-        {trendMovies.map(movie => (
-          <li key={movie.id}>
-            <Link
-              to={{
-                pathname: `movies/${movie.id}`,
-              }}
-            >
-              {movie.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {spinner && loader}
+      {trendMovies.length > 0 && (
+        <ul>
+          {trendMovies.map(movie => (
+            <li key={movie.id}>
+              <Link
+                to={{
+                  pathname: `movies/${movie.id}`,
+                }}
+              >
+                {movie.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
